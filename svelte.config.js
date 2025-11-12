61453e6c45cfe9c1bd63ab2dc79f60afdfa42b8d
Bret Hagen <bretm.hagen@gmail.com>
2025-11-11 18:37:16 -0800

    no more custom domain

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const dev = process.argv.includes('dev');
-
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -14,10 +12,7 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
-		}),
-		paths: {
-			base: dev ? '' : '/brethagen.dev'
-		}
+		})
 	}
 };
 
